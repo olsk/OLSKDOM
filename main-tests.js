08c1e54ec6d821d57987620e0b8d0dad4da794a6
Rosano Coutinho <pub@xeari.com>
2021-04-02 08:04:46 -0300

    Spec OLSKDOMMetadata

--- main-tests.js
+++ main-tests.js
@@ -1,3 +1,72 @@
 const { throws, deepEqual } = require('assert');
 
 const mod = require('./main.js');
+import { JSDOM } from 'jsdom';
+
+describe('OLSKDOMMetadata', function test_OLSKDOMMetadata () {
+
+	const _OLSKDOMMetadata = function (inputData) {
+		return mod.OLSKDOMMetadata(inputData, {
+			JSDOM: JSDOM.fragment,
+		});
+	};
+
+	it('throws if not string', function () {
+		throws(function () {
+			mod.OLSKDOMMetadata(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns object', function () {
+		deepEqual(_OLSKDOMMetadata(''), {});
+	});
+
+	it('sets title', function () {
+		const title = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<title>${ title }</title>`), {
+			title,
+		});
+	});
+
+	it('sets meta:property', function () {
+		const property = Math.random().toString();
+		const content = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<meta property="${ property }" content="${ content }" />`), {
+			[property]: content,
+		});
+	});
+
+	it('sets meta:itemprop', function () {
+		const itemprop = Math.random().toString();
+		const content = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<meta itemprop="${ itemprop }" content="${ content }" />`), {
+			[itemprop]: content,
+		});
+	});
+
+	it('sets link:itemprop', function () {
+		const itemprop = Math.random().toString();
+		const href = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<link itemprop="${ itemprop }" href="${ href }" />`), {
+			[itemprop]: href,
+		});
+	});
+
+	it('sets *:itemprop', function () {
+		const tag = uRandomElement('meta', 'link');
+		const itemprop = Math.random().toString();
+		const content = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<${ tag } itemprop="${ itemprop }" content="${ content }" />`), {
+			[itemprop]: content,
+		});
+	});
+
+	it('sets json-ld', function () {
+		const key = Math.random().toString();
+		const value = Math.random().toString();
+		deepEqual(_OLSKDOMMetadata(`<script type="application/ld+json">[{"${ key }":"${ value }"}]</script>`), {
+			[key]: value,
+		});
+	});
+
+});
--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const mod = {
 
+	OLSKDOMMetadata (inputData, debug = {}) {
+		if (typeof inputData !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		const doc = debug.JSDOM ? debug.JSDOM(inputData) : new DOMParser().parseFromString(inputData, 'text/html');
+
+		return Array.from(doc.querySelectorAll('[property],[itemprop]')).reduce(function (coll, item) {
+			const key = item.getAttribute('property') || item.getAttribute('itemprop');
+
+			if (!key) {
+				return coll;
+			}
+
+			return Object.assign(coll, {
+				[key]: item.getAttribute('content') || item.getAttribute('href'),
+			})
+		}, [].concat.apply([], [doc.querySelector('title') ? {
+			title: doc.querySelector('title').text,
+		} : {}, JSON.parse((doc.querySelector('script[type="application/ld+json"]') || {}).innerHTML || '[]')]).reduce(function (coll, item) {
+			return Object.assign(coll, item)
+		}, {}));
+	},
 	
 };
 
